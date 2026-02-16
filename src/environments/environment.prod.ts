4d0ef31cd8e9569a7d82473741e87138fc788000
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2026-02-16 17:48:55 +0530

    svg img size

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://localhost:8080/api/v1/user'
-  // baseUrl: 'http://15.184.181.76:8080/v1/user',
+  // baseUrl: 'http://localhost:3333/v1/user'
   baseUrl: 'https://api.dawajen.bh/v1/user',
 };
